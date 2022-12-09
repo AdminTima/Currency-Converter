c3ee527d66afa9439c956ce9c8a15cbdca44e1e9
AdminTima <tima.khilkevich@inbox.ru>
2022-12-09 15:35:23 +0300

    Added functionality to index view to convert currency and send response. Removed bugs from utility functions.

--- converter/utils.py
+++ converter/utils.py
@@ -16,20 +16,25 @@ def get_exchange_rates(pair):
     except Exception as err:
         print(err)
         return
-    data = response.json()
-    rate = data.get(pair, None)
+    json_resp = response.json()
+    data = json_resp.get("data", None)
+    rate = data.get(pair)
     return rate
 
 
 def convert(amount_of_money, rate):
     if rate:
-        return amount_of_money * rate
-    return None
+        result = float(amount_of_money) * float(rate)
+    else:
+        result = None
+    return result
 
 
 def get_result(from_currency, to_currency, amount_of_money):
     pair = from_currency + to_currency
+    print(pair)
     rate = get_exchange_rates(pair)
+    print(rate)
     return convert(amount_of_money, rate)
 
 
--- converter/views.py
+++ converter/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
+from .utils import get_result
+from .api import api_currency_list
 
 
 def index(request):
-    return render(request, "converter/index.html")
+    result = None
+    context = {"result": result, "currency_list": api_currency_list}
+    if request.method == "POST":
+        from_currency = request.POST["from_currency"]
+        to_currency = request.POST["to_currency"]
+        amount_of_money = request.POST["amount_of_money"]
+        print(from_currency, to_currency, amount_of_money)
+        result = get_result(from_currency, to_currency, amount_of_money)
+        print(result)
+        context["result"] = result
+
+    return render(request, "converter/index.html", context)
